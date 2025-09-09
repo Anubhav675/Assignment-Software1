a816a03cc63d2409a2816da7be8c51c57599878d
LOQ <anubhav675@gmail.com>
2025-09-09 23:57:15 +0300

    Module 5

---
+++ Module 5/assignment -4.py
@@ -0,0 +1,12 @@
+#Create an list
+name = []
+#Ask input from user for 5 times
+for i in range (5):
+    names = input("Enter the name of a city: ")
+    #Add the input from user to the list
+    name.append(names)
+print("\n\nThe cities you entered: ")
+#Print the list of cities serially
+for i in name:
+    print(i)
+
